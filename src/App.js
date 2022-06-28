4a21c9ab28d9f1770a99bf1d9028d79c4bdefbf6
Fernanda <fernanda_tiyomi@hotmail.com>
2022-06-28 20:07:03 -0300

    inserido Card Servicos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Inicial from './components/TelaInicial';
+import CardServicos from './components/CardServicos';
 
 
 class App extends React.Component {
@@ -18,7 +19,7 @@ class App extends React.Component {
       case "cadastro":
         return <p>Tela de Cadastro</p> //Alterar aqui quando a tela estiver pronta
       case "servicos":
-        return <p>Tela de Serviços</p> //Alterar aqui quando a tela estiver pronta
+        return  <CardServicos/> //Alterar aqui quando a tela estiver pronta
       case "carrinho":
         return <p>Tela do Carrinho</p> //Alterar aqui quando a tela estiver pronta
       case "detalhes":
