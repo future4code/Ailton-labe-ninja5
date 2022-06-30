7d567261245d5986d6f84a8a74e512c71a14e586
Fernanda <fernanda_tiyomi@hotmail.com>
2022-06-30 15:45:36 -0300

    atualizado a parte do design do card detalhes

--- src/components/DetalhesCard.js
+++ src/components/DetalhesCard.js
@@ -1,5 +1,31 @@
 import React from "react";
+import styled from "styled-components";
 import { getJobById } from "../services/requisicoes";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #ed6c2b94;
+  padding: 10px;
+  background-color: #ed6c2b94;
+  border-radius: 10px;
+  margin: 20px;
+
+  > p {
+    text-align: center;
+    margin-top: 30px;
+  }
+
+  > h2 {
+    text-align: center;
+  }
+`;
 
 export default class DetalhesCard extends React.Component {
   state = {
@@ -16,17 +42,25 @@ export default class DetalhesCard extends React.Component {
     });
   };
   render() {
-    
-   
-    return <div>
-
-        <p>{this.state.detalhes.title}</p>
-        <p>{this.state.detalhes.description}</p>
-        <p>{this.state.detalhes.price}</p>
-        <p>{this.state.detalhes.dueDate}</p>
-        <p>{this.state.detalhes.paymentMethods}</p>
-    
-        </div>
-   
+    return (
+      <Container>
+        <Card>
+          <h2>{this.state.detalhes.title}</h2>
+          <p>
+            <b>Descrição:</b> {this.state.detalhes.description}
+          </p>
+          <p>
+            <b>Valor: R$</b> {this.state.detalhes.price},00
+          </p>
+          <p>
+            <b>Prazo:</b> {this.state.detalhes.dueDate}
+          </p>
+          <p>
+            <b>Forma de pagamento: </b>
+            {this.state.detalhes.paymentMethods}
+          </p>
+        </Card>
+      </Container>
+    );
   }
 }
